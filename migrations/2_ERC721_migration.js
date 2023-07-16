cdcc0fae1fc6bb36e27928e5f6f5adea7a1c85df
Francesca Cella <francescacella96@gmail.com>
2023-07-16 15:03:14 +0200

    Remove proxy contract

--- migrations/2_ERC721_migration.js
+++ migrations/2_ERC721_migration.js
@@ -1,16 +1,9 @@
 const Plant = artifacts.require("Plant");
 const PlantReceiver = artifacts.require("PlantReceiver");
 const AnotherReceiver = artifacts.require("AnotherReceiver");
-const PlantUseCase = artifacts.require("PlantUseCase");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Plant);
   await deployer.deploy(PlantReceiver);
   await deployer.deploy(AnotherReceiver);
-  await deployer.deploy(
-    PlantUseCase,
-    Plant.address,
-    PlantReceiver.address,
-    AnotherReceiver.address
-  );
 };
--- test/ERC721Test.js
+++ test/ERC721Test.js
@@ -3,85 +3,79 @@ const truffleAssert = require('truffle-assertions');
 const Plant = artifacts.require("Plant");
 const PlantReceiver = artifacts.require("PlantReceiver");
 const AnotherReceiver = artifacts.require("AnotherReceiver");
-const PlantUseCase = artifacts.require("PlantUseCase");
 
 contract("ERC721Example", async (accounts) => {
   it("Should mint a plant", async () => {
-    await Plant.deployed();
-    const useCaseInstance = await PlantUseCase.deployed();
+    const instance = await Plant.deployed();
 
-    await useCaseInstance.mint(accounts[0]);
-    const plant = await useCaseInstance.getLastTokenId.call();
+    await instance.plantSeed(accounts[0]);
+    const plant = await instance.getLastPlanted.call();
 
-    assert(await useCaseInstance.exists.call(plant));
+    assert(await instance.isAlive.call(plant));
   });
 
   it("Should assign a plant", async () => {
-    await Plant.deployed();
-    const useCaseInstance = await PlantUseCase.deployed();
+    const instance = await Plant.deployed();
 
-    await useCaseInstance.mint(accounts[1]);
-    const plant = await useCaseInstance.getLastTokenId.call();
+    await instance.plantSeed(accounts[1]);
+    const plant = await instance.getLastPlanted.call();
 
-    assert.equal(await useCaseInstance.getBalance.call(accounts[1]), 1);
-    assert.equal(await useCaseInstance.getOwner.call(plant), accounts[1]);
+    assert.equal(await instance.balanceOf.call(accounts[1]), 1);
+    assert.equal(await instance.ownerOf.call(plant), accounts[1]);
   });
 
   it("Should uproot a plant", async () => {
-    await Plant.deployed();
-    const useCaseInstance = await PlantUseCase.deployed();
+    const instance = await Plant.deployed();
 
-    await useCaseInstance.mint(accounts[2]);
-    assert.equal(await useCaseInstance.getBalance.call(accounts[2]), 1);
+    await instance.plantSeed(accounts[2]);
+    assert.equal(await instance.balanceOf.call(accounts[2]), 1);
 
-    const plant = await useCaseInstance.getLastTokenId.call();
+    const plant = await instance.getLastPlanted.call();
 
-    await useCaseInstance.burn(plant);
-    assert.equal(await useCaseInstance.getBalance.call(accounts[2]), 0);
-    assert.isFalse(await useCaseInstance.exists.call(plant));
+    await instance.uproot(plant);
+    assert.equal(await instance.balanceOf.call(accounts[2]), 0);
+    assert.isFalse(await instance.isAlive.call(plant));
   });
 
   it("Should gift a plant", async () => {
-    const plantInstance = await Plant.deployed();
-    const useCaseInstance = await PlantUseCase.deployed();
+    const instance = await Plant.deployed();
 
-    await useCaseInstance.mint(accounts[3]);
-    const plant = await useCaseInstance.getLastTokenId.call();
-    assert.equal(await useCaseInstance.getBalance.call(accounts[3]), 1);
-    assert.equal(await useCaseInstance.getBalance.call(accounts[4]), 0);
+    await instance.plantSeed(accounts[3]);
+    const plant = await instance.getLastPlanted.call();
+    assert.equal(await instance.balanceOf.call(accounts[3]), 1);
+    assert.equal(await instance.balanceOf.call(accounts[4]), 0);
 
-    await plantInstance.gift(plant, accounts[4], { from: accounts[3] });
-    assert.equal(await useCaseInstance.getBalance.call(accounts[3]), 0);
-    assert.equal(await useCaseInstance.getBalance.call(accounts[4]), 1);
+    await instance.gift(plant, accounts[4], { from: accounts[3] });
+    assert.equal(await instance.balanceOf.call(accounts[3]), 0);
+    assert.equal(await instance.balanceOf.call(accounts[4]), 1);
   });
 
-  it("Should gift a plant with approval only", async () => {
-    const plantInstance = await Plant.deployed();
+  it("Should gift a plant with receiver only", async () => {
+    const instance = await Plant.deployed();
     await PlantReceiver.deployed();
     await AnotherReceiver.deployed();
-    const useCaseInstance = await PlantUseCase.deployed();
 
-    await useCaseInstance.mint(accounts[5]);
-    const plant = await useCaseInstance.getLastTokenId.call();
+    await instance.plantSeed(accounts[5]);
+    const plant = await instance.getLastPlanted.call();
 
     truffleAssert.reverts(
-      plantInstance.gift(plant, AnotherReceiver.address, {
+      instance.gift(plant, AnotherReceiver.address, {
         from: accounts[5],
       })
     );
     assert(
-      await plantInstance.gift(plant, PlantReceiver.address, {
+      await instance.gift(plant, PlantReceiver.address, {
         from: accounts[5],
       })
     );
     assert.equal(
-      await useCaseInstance.getBalance.call(PlantReceiver.address),
+      await instance.balanceOf.call(PlantReceiver.address),
       1
     );
     assert.equal(
-      await useCaseInstance.getBalance.call(AnotherReceiver.address),
+      await instance.balanceOf.call(AnotherReceiver.address),
       0
     );
-    assert.equal(await useCaseInstance.getBalance.call(accounts[5]), 0);
+    assert.equal(await instance.balanceOf.call(accounts[5]), 0);
   });
 });
