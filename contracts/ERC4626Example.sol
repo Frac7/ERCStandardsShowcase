6c24c8901a8e988e2366a4c411295ab09fd67a4b
Francesca Cella <francescacella96@gmail.com>
2023-10-08 20:27:25 +0200

    ERC4626 tests done

--- contracts/ERC4626Example.sol
+++ contracts/ERC4626Example.sol
@@ -12,17 +12,27 @@ contract TokenAsset is ERC20("Token", "TKN") {
 }
 
 contract TokenVault is ERC4626 {
-    constructor(TokenAsset _asset) ERC4626(_asset) ERC20("Token", "TKN") {}
+    constructor(
+        TokenAsset _asset,
+        string memory _name,
+        string memory _symbol
+    ) ERC4626(_asset) ERC20(_name, _symbol) {}
 
     function getBalance() public view returns (uint256) {
         return totalAssets();
     }
 
-    function mintSharesFromAssets(uint256 assets, address receiver) public returns (uint256 shares) {
+    function mintSharesFromAssets(
+        uint256 assets,
+        address receiver
+    ) public returns (uint256 shares) {
         return deposit(assets, receiver);
     }
 
-    function mintSharesWithoutAssets(uint256 shares, address receiver) public returns (uint256 assets) {
+    function mintSharesWithoutAssets(
+        uint256 shares,
+        address receiver
+    ) public returns (uint256 assets) {
         return mint(shares, receiver);
     }
 
@@ -34,7 +44,11 @@ contract TokenVault is ERC4626 {
         return withdraw(assets, receiver, owner);
     }
 
-    function withdrawBySharesAndBurnShares(uint256 shares, address receiver, address owner) public returns (uint256 assets) {
-      return redeem(shares, receiver, owner);
+    function withdrawBySharesAndBurnShares(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public returns (uint256 assets) {
+        return redeem(shares, receiver, owner);
     }
 }
--- test/ERC4626Test.js
+++ test/ERC4626Test.js
@@ -8,9 +8,11 @@ contract("ERC4626Example", async function (accounts) {
     this.underlyingAsset = await TokenAsset.new(totalSupply, {
       from: accounts[0],
     });
-
-    this.instance = await TokenVault.new(this.underlyingAsset.address);
-
+    this.instance = await TokenVault.new(
+      this.underlyingAsset.address,
+      "Token",
+      "TKN"
+    );
     await this.underlyingAsset.approve(this.instance.address, totalSupply, {
       from: accounts[0],
     });
@@ -18,55 +20,56 @@ contract("ERC4626Example", async function (accounts) {
 
   it("Should deposit - mint shares from assets", async function () {
     const assets = 10;
-
-    const shares = await this.instance.mintSharesFromAssets.call(
-      assets,
-      accounts[1],
-      { from: accounts[0] }
-    );
+    await this.instance.mintSharesFromAssets(assets, accounts[0], {
+      from: accounts[0],
+    });
 
     const balance = await this.instance.getBalance.call();
     assert.equal(assets, balance);
   });
 
   it("Should deposit - mint shares without assets", async function () {
-    const shares = 10;
-
-    const assets = await this.instance.mintSharesWithoutAssets.call(
-      shares,
-      accounts[1],
-      { from: accounts[0] }
-    );
+    const shares = 2;
+    await this.instance.mintSharesWithoutAssets(shares, accounts[0], {
+      from: accounts[0],
+    });
 
     const balance = await this.instance.getBalance.call();
-    assert.equal(assets, balance);
+    const assets = await this.instance.convertToAssets.call(shares);
+    assert.equal(assets.toNumber(), balance.toNumber());
   });
 
   it("Should withdraw - withdraw assets and burn shares", async function () {
     const assets = 10;
+    await this.instance.mintSharesFromAssets(assets, accounts[0], {
+      from: accounts[0],
+    });
 
-    await this.instance.withdrawByAssetsAndBurnShares.call(
+    await this.instance.withdrawByAssetsAndBurnShares(
       assets,
-      accounts[1],
+      accounts[0],
       accounts[0],
       { from: accounts[0] }
     );
 
     const balance = await this.instance.getBalance.call();
-    assert.equal(balance, totalSupply - assets);
+    assert.equal(balance.toNumber(), 0);
   });
 
   it("Should withdraw - withdraw assets by burning shares", async function () {
     const shares = 10;
+    await this.instance.mintSharesWithoutAssets(shares, accounts[0], {
+      from: accounts[0],
+    });
 
-    const assets = await this.instance.withdrawBySharesAndBurnShares.call(
+    await this.instance.withdrawBySharesAndBurnShares(
       shares,
-      accounts[1],
+      accounts[0],
       accounts[0],
       { from: accounts[0] }
     );
 
     const balance = await this.instance.getBalance.call();
-    assert.equal(balance, totalSupply - assets);
+    assert.equal(balance.toNumber(), 0);
   });
 });
